c5e31f7145d0d91e9b403548cbdc9ac7d3e6b94d
Allen Jude <ajallen142001@gamil.com>
2025-05-29 23:38:23 +0530

    commit

--- Background.js
+++ Background.js
@@ -1,26 +1,22 @@
 chrome.declarativeNetRequest.updateDynamicRules({
-    addRules : [
+    addRules: [
         {
-            id : 1,
-            priority : 1,
-            action : {
-                type : "block"
-            },
-            condition : {
-                urlFilter : "*://doubleclick.net/*",
-                resourceTypes : ["script", "image", "sub_frame"]
+            id: 1,
+            priority: 1,
+            action: { type: "block" },
+            condition: {
+                urlFilter: "*://doubleclick.net/*",
+                resourceTypes: ["script", "image", "sub_frame"]
             }
         },
         {
-            id : 2,
-            priority : 1,
-            action : {
-                type : "block"
-            },
-            condition : {
-                urlFilter : "*://googleadservices.com/*",
-                resourceTypes : ["script", "image", "sub_frame"]
+            id: 2,
+            priority: 1,
+            action: { type: "block" },
+            condition: {
+                urlFilter: "*://googleadservices.com/*",
+                resourceTypes: ["script", "image", "sub_frame"]
             }
         }
     ],
-})
+});
